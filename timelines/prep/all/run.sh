34508c68f8d78067b283cccf159931d7975bb09c
modelearth <model@dreamstudio.com>
2024-02-12 00:41:26 -0500

    Outputs from ./run.sh

--- timelines/prep/all/run.sh
+++ timelines/prep/all/run.sh
@@ -2,9 +2,11 @@
 # To run this bash script, enter command: ./run.sh
 echo 'Generating output for output/[year]/[year]_zcta_sm.csv.'
 #sqlite3 zcta.db < zcta.SQL.txt
+sqlite3 zcta.db < zcta_2012.SQL.txt> zcta.OUT.txt
 sqlite3 zcta.db < zcta_2013.SQL.txt> zcta.OUT.txt
 sqlite3 zcta.db < zcta_2014.SQL.txt> zcta.OUT.txt
 sqlite3 zcta.db < zcta_2015.SQL.txt> zcta.OUT.txt
 sqlite3 zcta.db < zcta_2016.SQL.txt> zcta.OUT.txt
+#sqlite3 zcta.db < zcta_2017.SQL.txt> zcta.OUT.txt
 rm zcta.db
 #echo 'Results sent to docs/data/2017/zcta_out.csv'
